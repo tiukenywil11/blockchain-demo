7ebbe59999f819bff222bec263b7fa77a3cc32d5
Kenywil Tiu <tiukenywil11@gmail.com>
2021-12-08 15:07:27 +0800

    Module 2 Step 8: Added replace chain api

--- module2/kennycoin.py
+++ module2/kennycoin.py
@@ -152,4 +152,15 @@ def connect_node():
                 'total_nodes': list(blockchain.nodes)}
     return jsonify(response), 201
 
+@app.route('/replace_chain', method=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': "The nodes had different chains so the chain was replaced by the longest chain",
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': "All good. The chain is the longest one." ,
+                    'actual_chain': blockchain.chain}
+    return jsonify(response), 200
+
 app.run(host = '0.0.0.0', port = 5000)
